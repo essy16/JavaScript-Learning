59be82baee83e74318833a773c07c2f0ca566f39
“Esther <essyc14@gmail.com>
2023-04-14 08:51:16 +0300

    Loops

---
+++ Loops/for.js
@@ -0,0 +1,5 @@
+//To learn more about for loops check out this Youtube video https://youtu.be/Kn06785pkJg
+for (let i = 0; i < 5; i++) {
+    console.log("Hey!You are doing so good in JavaScript!!")
+    
+}
