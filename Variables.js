1f9abf6f09f5a780366a45d41560c9c99370d6bc
“Esther <essyc14@gmail.com>
2023-04-14 07:35:50 +0300

    JavaScript Variables

---
+++ Variables.js
@@ -0,0 +1,15 @@
+// JavaScript Data types are used to identify the type of data that is stored inside a
+// variable during the script execution.
+// So JavaScript data types are basically for identification purposes to know what is
+// being stored in the variable, when it's stored, not before that.
+//Variable names should start with a letter and it should have no spaces.
+//Variable names are case-sensitive hence var name and var Name are completely different variables.
+// There are three types of variables:
+//Strings
+var str= "Web Program"
+
+//Booleans
+var isOn= true;
+
+//Numbers
+var age=25;
