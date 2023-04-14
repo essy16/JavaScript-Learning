8473e1d5be6da8f48f10288c61361af645d0e0f2
“Esther <essyc14@gmail.com>
2023-04-14 08:07:16 +0300

    Operators

---
+++ Operators.js
@@ -0,0 +1,82 @@
+// JavaScript operators are symbols which are used to assign values, compare values, perform arithmetic operations, and more. 
+// //There are four main types of operators
+// Arithmetic Operators
+// Addition
+var a = 4 + 6;
+
+// Subtraction
+var b = 5 + 7;
+
+// Multiplication
+var c = 5 * 8 ;
+
+// Modulus (Remainder)
+var x = 5 % 2;
+
+// Increment
+var x = 5;
+x++;
+var z = x;
+
+//Comparison operators
+//GIVEN THAT X=5
+
+//==	equal to	
+x == 8	//false
+x == 5	//true
+x == '5'	//true
+
+//===	equal value and equal type	
+x === 5	//true
+x === "5"	//false
+
+//!=	not equal	
+x != 8	//true
+
+//!==	not equal value or not equal type	
+x !== 5	//false
+x !== "5"	//true
+x !== 8	//true
+
+// >	greater than
+// <	less than
+// >=	greater than or equal to
+// <=	less than or equal to
+
+
+
+// Logical (or Relational) Operators
+// &&	and
+//GIVEN THAT x = 6 AND y = 3
+	(x < 10 && y > 1) //is true
+
+// ||	or	
+(x == 5 || y == 5) //is false
+
+//!	not
+	!(x == y) //is true
+
+
+
+//Assignment operators
+// The = assignment operator assigns a value to a variable.
+var x = 10;
+
+// The += assignment operator adds a value to a variable.
+x += 5;
+
+//The -= assignment operator subtracts a value from a variable.
+var x = 10;
+x -= 5;
+
+// The *= assignment operator multiplies a variable.
+var x = 10;
+x *= 5;
+
+// The /= assignment divides a variable.
+var x = 10;
+x /= 5;
+
+// The %= assignment operator assigns a remainder to a variable.
+var x = 10;
+x %= 5;
